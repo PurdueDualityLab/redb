fb3f421ba238e430425d89f102ba19287fc39c27
Charlie Sale <softwaresale01@gmail.com>
2021-12-25 20:17:59 -0500

    librereuse: add function for reading a collection of clusters

--- librereuse/db/pattern_reader.cc
+++ librereuse/db/pattern_reader.cc
@@ -38,3 +38,29 @@ std::vector<std::string> rereuse::db::read_patterns_from_path(const std::string 
         return std::vector<std::string>();
     }
 }
+
+std::vector<std::unique_ptr<rereuse::db::Cluster>> rereuse::db::read_semantic_clusters(const std::string &file_path) {
+    std::ifstream seed_file(file_path);
+    nlohmann::json cluster_array;
+    if (seed_file.is_open()) {
+        seed_file >> cluster_array;
+        seed_file.close();
+    } else {
+        return {};
+    }
+
+    std::vector<std::unique_ptr<rereuse::db::Cluster>> clusters;
+
+    // Iterate over clusters
+    for (const auto &cluster_patterns : cluster_array) {
+        std::unordered_set<std::string> patterns;
+        for (const auto &cluster_pattern : cluster_patterns) {
+            patterns.insert(cluster_pattern.get<std::string>());
+        }
+
+        auto cluster = std::make_unique<rereuse::db::Cluster>(patterns);
+        clusters.push_back(std::move(cluster));
+    }
+
+    return clusters;
+}
--- librereuse/db/pattern_reader.h
+++ librereuse/db/pattern_reader.h
@@ -8,10 +8,13 @@
 #include <istream>
 #include <string>
 #include <vector>
+#include <memory>
+#include "cluster.h"
 
 namespace rereuse::db {
     std::vector<std::string> read_patterns(std::istream &input_stream);
     std::vector<std::string> read_patterns_from_path(const std::string &path);
+    std::vector<std::unique_ptr<rereuse::db::Cluster>> read_semantic_clusters(const std::string &file_path);
 }
 
 #endif //_PATTERNREADER_H
