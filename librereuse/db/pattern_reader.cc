541802133ab5688e72eb0d3d61cf54b535d529ac
Charlie Sale <softwaresale01@gmail.com>
2022-02-14 16:19:53 -0500

    clusters: implement C&S semantic clustering algo

--- librereuse/db/pattern_reader.cc
+++ librereuse/db/pattern_reader.cc
@@ -35,8 +35,40 @@ std::vector<std::string> rereuse::db::read_patterns_from_path(const std::string 
         db_file.close();
         return patterns;
     } else {
-        return std::vector<std::string>();
+        return {};
     }
+}
+
+static inline void trim_string_end(std::string &str) {
+    str.erase(std::find_if(str.rbegin(), str.rend(),
+                           [](unsigned char c) { return !std::isspace(c); }).base(), str.end());
+}
+
+std::unordered_map<unsigned long, std::string> rereuse::db::read_patterns_id_pairs(std::istream &input_stream) {
+    re2::RE2 parser("^(\\d+)\\s+(.*)$");
+    std::string line;
+    std::unordered_map<unsigned long, std::string> patterns;
+    while (std::getline(input_stream, line)) {
+        unsigned long id;
+        std::string pattern;
+        if (re2::RE2::FullMatch(line, parser, &id, &pattern)) {
+            // Trim any whitespace on the end of pattern
+            trim_string_end(pattern);
+            patterns[id] = pattern;
+        }
+    }
+
+    return patterns;
+}
+
+std::unordered_map<unsigned long, std::string> rereuse::db::read_patterns_id_pairs_path(const std::string &path) {
+    std::ifstream in_file(path);
+    if (!in_file)
+        throw std::runtime_error("Could not open patterns file");
+
+    auto patterns = read_patterns_id_pairs(in_file);
+    in_file.close();
+    return patterns;
 }
 
 std::vector<std::unique_ptr<rereuse::db::Cluster>> rereuse::db::read_semantic_clusters(const std::string &file_path) {
